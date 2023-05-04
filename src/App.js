532ede8fe7f40dae0e3b75c8ad3b0654a5df2502
Ankitadevatale2305 <126754673+Ankitadevatale2305@users.noreply.github.com>
2023-05-04 19:15:06 +0530

    Created file TextForm.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm' ;
 import './style.css';
 
 let name = 'Ankita';
@@ -8,6 +9,8 @@ export default function App() {
   return (
     <>
        <Navbar title="TextUtils" aboutText="About Us" contactText="Contact Us" /> 
+       <div className="container my-3" ></div>
+       <TextForm  heading="Enter Your Text"/>
       {/* <Navbar /> */}
     </>
   );
---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+  const [text, setText] = useState('Enter Text ');
+  const handleLowClicked = () => {
+    console.log('LowerCase Was clicked');
+    let newTextTwo = text.toLowerCase();
+    setText(newTextTwo);
+  };
+  const handleUpClicked = () => {
+    console.log('Uppercase was clicked' + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    // setText('you have clicked handle up clicked');
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <div className="mb-3">
+        <h1>{props.heading} </h1>
+        <textarea
+          className="form-control"
+          value={text}
+          rows="10"
+          onChange={handleOnChange}
+        ></textarea>
+        <button className="btn btn-primary my-3" onClick={handleUpClicked}>
+          Convert to UpperCase
+        </button>
+        <button
+          className="btn btn-primary my-3 mx-3"
+          onClick={handleLowClicked}
+        >
+          Convert to LowerCase
+        </button>
+      </div>
+    </div>
+  );
+}
