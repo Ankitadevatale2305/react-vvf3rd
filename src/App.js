aa58af6e1dab4efd27d8516f45531acf7c33b645
Ankitadevatale2305 <126754673+Ankitadevatale2305@users.noreply.github.com>
2023-05-05 14:32:22 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm' ;
+import About from './components/About' ;
+
+
 import './style.css';
 
 let name = 'Ankita';
@@ -11,7 +14,8 @@ export default function App() {
        <Navbar title="TextUtils" aboutText="About Us" contactText="Contact Us" /> 
        <div className="container my-3" ></div>
        <TextForm  heading="Enter Your Text"/>
-      {/* <Navbar /> */}
+       <About />
+  
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,29 +15,48 @@ export default function TextForm(props) {
     setText(newText);
     // setText('you have clicked handle up clicked');
   };
-
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
   return (
-    <div className="container">
-      <div className="mb-3">
-        <h1>{props.heading} </h1>
-        <textarea
-          className="form-control"
-          value={text}
-          rows="10"
-          onChange={handleOnChange}
-          placeholder="Please Enter Text Here"
-        ></textarea>
-        <button className="btn btn-primary my-3" onClick={handleUpClicked}>
-          Convert to UpperCase
-        </button>
-        <button className="btn btn-info my-3 mx-3" onClick={handleLowClicked}>
-          Convert to LowerCase
-        </button>
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading} </h1>
+          <textarea
+            className="form-control"
+            value={text}
+            rows="10"
+            onChange={handleOnChange}
+            placeholder="Please Enter Text Here"
+          ></textarea>
+          <button className="btn btn-primary my-3" onClick={handleUpClicked}>
+            Convert to UpperCase
+          </button>
+          <button className="btn btn-info my-3 mx-3" onClick={handleLowClicked}>
+            Convert to LowerCase
+          </button>
+          <button className="btn btn-secondary my-3 mx-3" onClick={speak}>
+            Text To Speech
+          </button>
+        </div>
       </div>
-    </div>
+
+      <div className="container">
+        <h2>Your Text Summry</h2>
+        <p>
+          {text.split(' ').length} Words and {text.length}Characters
+        </p>
+        <p> {0.008 * text.split(' ').length} Minutes Reading Time</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
